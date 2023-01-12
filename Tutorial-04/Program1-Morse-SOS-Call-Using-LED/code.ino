8ae8e83b812bd24ab4efcc558f8d20e48630d96e
SHUHBHAM KUMAR <88201167+shubham3279@users.noreply.github.com>
2023-01-12 17:17:51 +0530

    Create code.ino

---
+++ Tutorial-04/Program1-Morse-SOS-Call-Using-LED/code.ino
@@ -0,0 +1,58 @@
+int outPin = 12; //output PIoutPin
+int dotDelay = 200;
+int dashDelay = 600;
+int longDelay = 2000;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(outPin, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  // ...
+  digitalWrite(outPin,HIGH);
+  delay(dotDelay);
+  digitalWrite(outPin, LOW);
+  delay(dotDelay);
+  digitalWrite(outPin,HIGH);
+  delay(dotDelay);
+  digitalWrite(outPin, LOW);
+  delay(dotDelay);
+  digitalWrite(outPin,HIGH);
+  delay(dotDelay);
+  digitalWrite(outPin, LOW);
+  delay(dotDelay);
+
+  // ---
+  digitalWrite(outPin,HIGH);
+  delay(dashDelay);
+  digitalWrite(outPin, LOW);
+  delay(dashDelay);
+  digitalWrite(outPin,HIGH);
+  delay(dashDelay);
+  digitalWrite(outPin, LOW);
+  delay(dashDelay);
+  digitalWrite(outPin,HIGH);
+  delay(dashDelay);
+  digitalWrite(outPin, LOW);
+  delay(dashDelay);
+
+  // ...
+  digitalWrite(outPin,HIGH);
+  delay(dotDelay);
+  digitalWrite(outPin, LOW);
+  delay(dotDelay);
+  digitalWrite(outPin,HIGH);
+  delay(dotDelay);
+  digitalWrite(outPin, LOW);
+  delay(dotDelay);
+  digitalWrite(outPin,HIGH);
+  delay(dotDelay);
+  digitalWrite(outPin, LOW);
+  delay(dotDelay);
+
+  //additional delay
+  delay(longDelay);
+}
