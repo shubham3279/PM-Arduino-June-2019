07d23b8a28a4d87cc505f7ad4c4d6db5b6dac20e
shubham3279 <shubhambseb123@gmail.com>
2023-01-12 19:49:40 +0530

    added a1 of t6

---
+++ Tutorial-06/Assignemt-01/sketch.ino
@@ -0,0 +1,274 @@
+int outPin1 = 2,
+    outPin2 = 3,
+    outPin3 = 4,
+    outPin4 = 5,
+    outPin5 = 6,
+    delayTime = 2000;
+
+void setup(){
+  pinMode(outPin1,OUTPUT);
+  pinMode(outPin2,OUTPUT);
+  pinMode(outPin3,OUTPUT);
+  pinMode(outPin4,OUTPUT);
+  pinMode(outPin5,OUTPUT);
+}
+
+void loop(){
+  //0
+  digitalWrite(outPin1, LOW);
+  digitalWrite(outPin2, LOW);
+  digitalWrite(outPin3, LOW);
+  digitalWrite(outPin4, LOW);
+  digitalWrite(outPin5, LOW);
+  delay(delayTime);
+
+  //1
+  digitalWrite(outPin1, LOW);
+  digitalWrite(outPin2, LOW);
+  digitalWrite(outPin3, LOW);
+  digitalWrite(outPin4, LOW);
+  digitalWrite(outPin5, HIGH);
+  delay(delayTime);
+
+  //2
+  digitalWrite(outPin1, LOW);
+  digitalWrite(outPin2, LOW);
+  digitalWrite(outPin3, LOW);
+  digitalWrite(outPin4, HIGH);
+  digitalWrite(outPin5, LOW);
+  delay(delayTime);
+
+  //3
+  digitalWrite(outPin1, LOW);
+  digitalWrite(outPin2, LOW);
+  digitalWrite(outPin3, LOW);
+  digitalWrite(outPin4, HIGH);
+  digitalWrite(outPin5, HIGH);
+  delay(delayTime);
+
+  //4
+  digitalWrite(outPin1, LOW);
+  digitalWrite(outPin2, LOW);
+  digitalWrite(outPin3, HIGH);
+  digitalWrite(outPin4, LOW);
+  digitalWrite(outPin5, LOW);
+  delay(delayTime);
+
+  //5
+  digitalWrite(outPin1, LOW);
+  digitalWrite(outPin2, LOW);
+  digitalWrite(outPin3, HIGH);
+  digitalWrite(outPin4, LOW);
+  digitalWrite(outPin5, HIGH);
+  delay(delayTime);
+
+  //6
+  digitalWrite(outPin1, LOW);
+  digitalWrite(outPin2, LOW);
+  digitalWrite(outPin3, HIGH);
+  digitalWrite(outPin4, HIGH);
+  digitalWrite(outPin5, LOW);
+  delay(delayTime);
+
+  //7
+  digitalWrite(outPin1, LOW);
+  digitalWrite(outPin2, LOW);
+  digitalWrite(outPin3, HIGH);
+  digitalWrite(outPin4, HIGH);
+  digitalWrite(outPin5, HIGH);
+  delay(delayTime);
+
+  //8
+  digitalWrite(outPin1, LOW);
+  digitalWrite(outPin2, HIGH);
+  digitalWrite(outPin3, LOW);
+  digitalWrite(outPin4, LOW);
+  digitalWrite(outPin5, LOW);
+  delay(delayTime);
+
+  //9
+  digitalWrite(outPin1, LOW);
+  digitalWrite(outPin2, HIGH);
+  digitalWrite(outPin3, LOW);
+  digitalWrite(outPin4, LOW);
+  digitalWrite(outPin5, HIGH);
+  delay(delayTime);
+
+  //10
+  digitalWrite(outPin1, LOW);
+  digitalWrite(outPin2, HIGH);
+  digitalWrite(outPin3, LOW);
+  digitalWrite(outPin4, HIGH);
+  digitalWrite(outPin5, LOW);
+  delay(delayTime);
+
+  //11
+  digitalWrite(outPin1, LOW);
+  digitalWrite(outPin2, HIGH);
+  digitalWrite(outPin3, LOW);
+  digitalWrite(outPin4, HIGH);
+  digitalWrite(outPin5, HIGH);
+  delay(delayTime);
+
+  //12
+  digitalWrite(outPin1, LOW);
+  digitalWrite(outPin2, HIGH);
+  digitalWrite(outPin3, HIGH);
+  digitalWrite(outPin4, LOW);
+  digitalWrite(outPin5, LOW);
+  delay(delayTime);
+
+  //13
+  digitalWrite(outPin1, LOW);
+  digitalWrite(outPin2, HIGH);
+  digitalWrite(outPin3, HIGH);
+  digitalWrite(outPin4, LOW);
+  digitalWrite(outPin5, HIGH);
+  delay(delayTime);
+
+  //14
+  digitalWrite(outPin1, LOW);
+  digitalWrite(outPin2, HIGH);
+  digitalWrite(outPin3, HIGH);
+  digitalWrite(outPin4, HIGH);
+  digitalWrite(outPin5, LOW);
+  delay(delayTime);
+
+  //15
+  digitalWrite(outPin1, LOW);
+  digitalWrite(outPin2, HIGH);
+  digitalWrite(outPin3, HIGH);
+  digitalWrite(outPin4, HIGH);
+  digitalWrite(outPin5, HIGH);
+  delay(delayTime);
+
+  //16
+  digitalWrite(outPin1, HIGH);
+  digitalWrite(outPin2, LOW);
+  digitalWrite(outPin3, LOW);
+  digitalWrite(outPin4, LOW);
+  digitalWrite(outPin5, LOW);
+  delay(delayTime);
+
+  //17
+  digitalWrite(outPin1, HIGH);
+  digitalWrite(outPin2, LOW);
+  digitalWrite(outPin3, LOW);
+  digitalWrite(outPin4, LOW);
+  digitalWrite(outPin5, HIGH);
+  delay(delayTime);
+
+  //18
+  digitalWrite(outPin1, HIGH);
+  digitalWrite(outPin2, LOW);
+  digitalWrite(outPin3, LOW);
+  digitalWrite(outPin4, HIGH);
+  digitalWrite(outPin5, LOW);
+  delay(delayTime);
+
+  //19
+  digitalWrite(outPin1, HIGH);
+  digitalWrite(outPin2, LOW);
+  digitalWrite(outPin3, LOW);
+  digitalWrite(outPin4, HIGH);
+  digitalWrite(outPin5, HIGH);
+  delay(delayTime);
+
+  //20
+  digitalWrite(outPin1, HIGH);
+  digitalWrite(outPin2, LOW);
+  digitalWrite(outPin3, HIGH);
+  digitalWrite(outPin4, LOW);
+  digitalWrite(outPin5, LOW);
+  delay(delayTime);
+
+  //21
+  digitalWrite(outPin1, HIGH);
+  digitalWrite(outPin2, LOW);
+  digitalWrite(outPin3, HIGH);
+  digitalWrite(outPin4, LOW);
+  digitalWrite(outPin5, HIGH);
+  delay(delayTime);
+
+  //22
+  digitalWrite(outPin1, HIGH);
+  digitalWrite(outPin2, LOW);
+  digitalWrite(outPin3, HIGH);
+  digitalWrite(outPin4, HIGH);
+  digitalWrite(outPin5, LOW);
+  delay(delayTime);
+
+  //23
+  digitalWrite(outPin1, HIGH);
+  digitalWrite(outPin2, LOW);
+  digitalWrite(outPin3, HIGH);
+  digitalWrite(outPin4, HIGH);
+  digitalWrite(outPin5, HIGH);
+  delay(delayTime);
+
+  //24
+  digitalWrite(outPin1, HIGH);
+  digitalWrite(outPin2, HIGH);
+  digitalWrite(outPin3, LOW);
+  digitalWrite(outPin4, LOW);
+  digitalWrite(outPin5, LOW);
+  delay(delayTime);
+
+  //25
+  digitalWrite(outPin1, HIGH);
+  digitalWrite(outPin2, HIGH);
+  digitalWrite(outPin3, LOW);
+  digitalWrite(outPin4, LOW);
+  digitalWrite(outPin5, HIGH);
+  delay(delayTime);
+
+  //26
+  digitalWrite(outPin1, HIGH);
+  digitalWrite(outPin2, HIGH);
+  digitalWrite(outPin3, LOW);
+  digitalWrite(outPin4, HIGH);
+  digitalWrite(outPin5, LOW);
+  delay(delayTime);
+
+  //27
+  digitalWrite(outPin1, HIGH);
+  digitalWrite(outPin2, HIGH);
+  digitalWrite(outPin3, LOW);
+  digitalWrite(outPin4, HIGH);
+  digitalWrite(outPin5, HIGH);
+  delay(delayTime);
+
+  //28
+  digitalWrite(outPin1, HIGH);
+  digitalWrite(outPin2, HIGH);
+  digitalWrite(outPin3, HIGH);
+  digitalWrite(outPin4, LOW);
+  digitalWrite(outPin5, LOW);
+  delay(delayTime);
+
+  //29
+  digitalWrite(outPin1, HIGH);
+  digitalWrite(outPin2, HIGH);
+  digitalWrite(outPin3, HIGH);
+  digitalWrite(outPin4, LOW);
+  digitalWrite(outPin5, HIGH);
+  delay(delayTime);
+
+  //30
+  digitalWrite(outPin1, HIGH);
+  digitalWrite(outPin2, HIGH);
+  digitalWrite(outPin3, HIGH);
+  digitalWrite(outPin4, HIGH);
+  digitalWrite(outPin5, LOW);
+  delay(delayTime);
+
+  //31
+  digitalWrite(outPin1, HIGH);
+  digitalWrite(outPin2, HIGH);
+  digitalWrite(outPin3, HIGH);
+  digitalWrite(outPin4, HIGH);
+  digitalWrite(outPin5, HIGH);
+  delay(delayTime);
+  
+
+}
